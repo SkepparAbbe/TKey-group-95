137c9e2ce0e6a9ac8bc1d0fb10cb0d7d1484aae8
SkepparAbbe <albske12@gmail.com>
2025-03-11 17:05:22 +0100

    Added check for if userid set in session is not in database.

--- Web-application/flask_app/auth.py
+++ Web-application/flask_app/auth.py
@@ -7,14 +7,17 @@ bp = Blueprint('auth', __name__, url_prefix='/auth')
 @bp.before_app_request
 def load_logged_in_user():
     user_id = session.get('user_id')
-
+    print("Current user id is " + str(user_id))
     if user_id is None:
         g.user = None
     else:
         user = get_db().execute(
             'SELECT username FROM user WHERE id = ?', (user_id,)
         ).fetchone()
-        g.user = user['username'] 
+        if user is None:
+            pass
+        else:
+            g.user = user['username'] 
 
 def login_required(view):
     @functools.wraps(view)
