fed9d1de36f2256a6c9a737a1c0201c8645868e9
Yeenasour <wilmer.greppe@gmail.com>
2025-04-04 14:42:49 +0200

    enabled hot-reload for development

--- Web-application/flask_app/__init__.py
+++ Web-application/flask_app/__init__.py
@@ -20,9 +20,7 @@ def create_app(test_config=None):
     app = Flask(__name__, instance_relative_config=True)
     app.config.from_mapping(
         SECRET_KEY=os.getenv("FLASK_SECRET_KEY"),
-        DATABASE=os.environ.get('DATABASE_URL'),
-        FLASK_RUN_HOST='localhost',
-        FLASK_RUN_PORT=8000
+        DATABASE=os.environ.get('DATABASE_URL')
     )
 
     # Creates csrf protection object that forms from flask-wtf needs.
