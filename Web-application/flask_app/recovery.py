eba44576e5acdcc54310db178e6656be97be279a
Amanch200309 <Amanch0308@hotmail.com>
2025-04-05 15:25:57 +0200

    Added seed phrase during registration
    Co-authored-by: themaxlevin <themaxlevin@users.noreply.github.com>

--- Web-application/flask_app/recovery.py
+++ Web-application/flask_app/recovery.py
@@ -2,6 +2,9 @@ from mnemonic import Mnemonic
 import hashlib
 import os
 
+# Higher iteration count yields longer time to successful brute force attempts
+iterations = 100000
+
 # Generate a mnemonic phrase 
 def generate_mnemonic():
     mnemo = Mnemonic("english") # Creates a Mnemonic object 
@@ -9,7 +12,7 @@ def generate_mnemonic():
     return mnemonic
 
 # Convert the mnemonic phrase to a seed phrase
-def conv_seed(mnemonic):
+def convert_to_seed(mnemonic):
     mnemo = Mnemonic("english")
     seed = mnemo.to_seed(mnemonic, passphrase="")
     return seed.hex()
@@ -17,7 +20,12 @@ def conv_seed(mnemonic):
 # Hashes seed phrase and generates salt
 def hash_seed(seed):
         salt = os.urandom(16)
-        return hashlib.pbkdf2_hmac('sha256', seed.encode(), salt, 100000).hex() , salt.hex()
+        return hashlib.pbkdf2_hmac('sha256', seed.encode(), salt, iterations).hex() , salt.hex()
+
+def verify_mnemonic(stored_hash, salt, mnemonic):
+    new_seed = convert_to_seed(mnemonic)
+    new_hash = hashlib.pbkdf2_hmac('sha256', new_seed.encode(), bytes.fromhex(salt), iterations).hex()
+    return stored_hash == new_hash
 
 
 '''
@@ -25,11 +33,16 @@ if __name__ == "__main__":
     nemonic = generate_mnemonic()   
     print("Mnemonic Phrase: ", nemonic)
 
-    seed = conv_seed(nemonic)  
+    seed = convert_to_seed(nemonic)  
     print("Seed Phrase: ", seed)
     
     hashed_seed, salt = hash_seed(seed)
     print("Hashed Seed: ", hashed_seed)
     print("Salt: ", salt)
-
+    
+    input_mnemonic = input("Enter the mnemonic phrase to verify: ")
+    if verify_mnemonic(hashed_seed, salt, input_mnemonic):
+        print("Mnemonic verified successfully!")
+    else:
+        print("Mnemonic verification failed.")
 '''
--- Web-application/schema.sql
+++ Web-application/schema.sql
@@ -4,5 +4,8 @@ CREATE TABLE "user" (
     id SERIAL PRIMARY KEY,
     username TEXT UNIQUE NOT NULL,
     publickey TEXT UNIQUE NOT NULL,
-    secret TEXT NOT NULL
+    secret TEXT NOT NULL,
+    salt TEXT NOT NULL,
+    hash TEXT NOT NULL
+
 );
